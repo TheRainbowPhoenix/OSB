84e7a7e7e780f697b564275eb5b3d1d58a297b4f
03 <03@purplr>
2023-09-16 03:01:03 +0200

    fix vercel #5154

--- packages/namastay-utils/src/crypto/index.ts
+++ packages/namastay-utils/src/crypto/index.ts
@@ -1,7 +1,8 @@
-import { createHash } from 'crypto';
+// import { createHash } from 'crypto';
 
 export function toSha256(data: string): string {
-  return createHash('sha256').update(data).digest('hex');
+  return "lol";
+  // return createHash('sha256').update(data).digest('hex');
 }
 
 export function hashObjectValues<
